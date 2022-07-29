1749273a0400d9a273d7ac9ae8aeb0b8e8706f8d
GSindusha <sindusha023@gmail.com>
2022-07-29 18:56:22 +0530

    my project

--- EmployeeMicroservice/Controllers/EmployeeController.cs
+++ EmployeeMicroservice/Controllers/EmployeeController.cs
@@ -27,12 +27,7 @@ namespace EmployeeMicroservice.Controllers
 
         }
 
-        private readonly IConfiguration _configuration=default!;
-        public EmployeeController(IConfiguration configuration)
-        {
-            
-            this._configuration = configuration;
-        }
+        
         // GET: api/<EmployeeController>
         [HttpGet]
         public IEnumerable<Employee> Get()
@@ -48,68 +43,8 @@ namespace EmployeeMicroservice.Controllers
         }
 
 
-        [HttpPost]
-        [Route("Registration")]
-        public async Task<ActionResult<string>> Registration(Employee request)
-        {
-
-            var employee = new Employee();
-
-            employee.Name = request.Name;
-            employee.Gender = request.Gender;
-            employee.Age = request.Age;
-            employee.Salary = request.Salary;
-            employee.Phone = request.Phone;
-            employee.EmailId = request.EmailId;
-            employee.Password = request.Password;
-            employee.Type = request.Type;
-
-
-            db.Employee.Add(employee);
-            await db.SaveChangesAsync();
-            return Ok("User Created!!");
-        }
-
-        [HttpPost("login")]
-        public async Task<ActionResult<string>> Login(Employee employee)
-        {
-            List<Employee> user = await db.Employee.Where(o => o.EmailId == employee.EmailId).ToListAsync();
-            if (user[0].EmailId != employee.EmailId)
-            {
-                return BadRequest("User not found.");
-            }
-
-            string token = CreateToken(user[0]);
-
-            return Ok(token);
-        }
-
-        private string CreateToken(Employee employee)
-        {
-            // throw new NotImplementedException();
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, employee.EmailId),
-                new Claim(ClaimTypes.Role, "Admin")
-            };
-
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
-                _configuration.GetSection("AppSettings:Token").Value));
-
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds);
-
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return jwt;
-
-        }
-
-        //Post method - ResetPassword
+       
+      
 
         // POST api/<EmployeeController>
         [HttpPost]
@@ -128,29 +63,7 @@ namespace EmployeeMicroservice.Controllers
 
         }
 
-        // PUT api/<EmployeeController>/5
-        [HttpPut("{id}")]    //no update feature in ui
-        //public void Put(int id, [FromBody] string value)
-        public async Task<ActionResult<List<Employee>>> UpdateEmployee(Employee employee)
-        {
-            var dbEmployee= await db.Employee.FindAsync(employee.EmployeeId);
-            if (dbEmployee == null) { return BadRequest("Employee Not Found"); }
-
        
-            dbEmployee.Name = employee.Name;
-            dbEmployee.Gender = employee.Gender;
-            dbEmployee.Age = employee.Age;
-            dbEmployee.Salary = employee.Salary;
-            dbEmployee.Phone = employee.Phone;
-            dbEmployee.EmailId = employee.EmailId;
-            dbEmployee.Password = employee.Password;
-            dbEmployee.Type = employee.Type;
-
-
-            await db.SaveChangesAsync();
-            return Ok(await db.Employee.ToListAsync());
-
-        }
 
         // DELETE api/<EmployeeController>/5
         [HttpDelete("{id}")]
